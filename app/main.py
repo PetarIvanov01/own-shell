dbdac17716c2b4a98a983c3a42868222f8a6b61f
PetarIvanov01 <petarivanov0122@gmail.com>
2025-05-31 15:11:27 +0300

    echoing the string

--- app/main.py
+++ app/main.py
@@ -12,7 +12,14 @@ def main():
         if command.startswith("exit") and command.endswith("0"):
             sys.exit(0)
 
-        print(f"{command}: command not found")
+        if command.startswith("echo"):
+            output = command.split(" ", 1)
+            if len(output) > 1:
+                print(output)
+            else:
+                print("")
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
