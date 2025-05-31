0ba49f80ead4bdd1a92cc6e2538c16fdd2a48bfd
PetarIvanov01 <petarivanov0122@gmail.com>
2025-05-31 15:06:22 +0300

    check for exit

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ def main():
         sys.stdout.write("$ ")
 
         command = input()
+
+        if command.startswith("exit") and command.endswith("0"):
+            sys.exit(0)
+
         print(f"{command}: command not found")
 
 
